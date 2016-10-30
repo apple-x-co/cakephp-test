ea0a18fa41851f418c9caaaa748a650578f48857
apple-x-co <apple.x.co@gmail.com>
2016-10-30 18:25:36 +0900

    commit

---
+++ app/src/Controller/HelloController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kouhei
+ * Date: 2016/10/30
+ * Time: 18:09
+ */
+
+use App\Controller\AppController;
+
+class HelloController extends AppController {
+
+    public function index() {
+
+        $this->set('hellow_world', 'HELLO');
+
+    }
+
+
+}
--- config/routes.php
+++ config/routes.php
@@ -56,6 +56,8 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
+    $routes->connect('/hello', ['controller' => 'Hello', 'action' => 'index']);
+
     /**
      * Connect catchall routes for all controllers.
      *
