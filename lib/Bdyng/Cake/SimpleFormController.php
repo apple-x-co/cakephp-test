9f0870b7af2f43905fa58674d0788bf44bd76aeb
apple-x-co <apple.x.co@gmail.com>
2016-11-03 16:53:21 +0900

    commit

--- lib/Bdyng/Cake/SimpleFormController.php
+++ lib/Bdyng/Cake/SimpleFormController.php
@@ -27,7 +27,7 @@ abstract class SimpleFormController extends AppController {
         print_r($this->request->data('__confirm'));
         print_r($this->request->data('name'));
         print_r($this->request->data);
-        print_r(array_key_exists('__confirm', $this->request->data('name')));
+        print_r(array_key_exists('__confirm', $this->request->data));
 
         $mode = $this->request->param('__mode');
         if (!array_key_exists('__mode', $this->request->params)) {
