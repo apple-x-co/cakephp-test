8d81c8e2194a9b535327a9f13417c902d784e519
apple-x-co <apple.x.co@gmail.com>
2016-11-03 18:45:34 +0900

    commit

--- src/Controller/HelloFormController.php
+++ src/Controller/HelloFormController.php
@@ -18,30 +18,3 @@ class HelloFormController extends SimpleFormController {
     }
 
 }
-
-//namespace App\Controller;
-//
-//use App\Form\HelloForm;
-//
-//class HelloFormController extends  AppController {
-//
-//    public function index() {
-//
-//        $hello_form = new HelloForm();
-//
-//        if ($this->request->is('post')) {
-//            if ($hello_form->execute($this->request->data)) {
-//                $this->Flash->success('NOW!!');
-//            }
-//            else {
-//                $this->Flash->error('ERROR!!');
-//            }
-//
-//            $this->render('confirm');
-//        }
-//
-//        $this->set('hello_form', $hello_form);
-//
-//    }
-//
-//}
