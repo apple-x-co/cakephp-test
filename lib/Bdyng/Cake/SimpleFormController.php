51b9dbf4823052adf3f7521048a05c6d7b0b0bd3
apple-x-co <apple.x.co@gmail.com>
2016-11-03 12:59:27 +0900

    commit

--- lib/Bdyng/Cake/SimpleFormController.php
+++ lib/Bdyng/Cake/SimpleFormController.php
@@ -23,7 +23,7 @@ abstract class SimpleFormController extends AppController {
 
     public function index() {
 
-        $this->Flash->success($this->request->param('__mode'));
+        $this->Flash->success(array_key_exists('__mode', $this->request->params));
 
         $mode = $this->request->param('__mode');
         if (!array_key_exists('__mode', $this->request->params)) {
