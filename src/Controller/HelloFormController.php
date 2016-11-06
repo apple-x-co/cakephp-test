30b428fff63223f35f0b35197051e8e2de0d1678
apple-x-co <apple.x.co@gmail.com>
2016-11-06 13:33:55 +0900

    commit

--- src/Controller/HelloFormController.php
+++ src/Controller/HelloFormController.php
@@ -8,13 +8,26 @@
 
 namespace App\Controller;
 
-use Bdyng\Cake\SimpleFormController;
+use Cake\Mailer\Email;
+
 use App\Form\HelloForm;
+
+use Bdyng\Cake\SimpleFormController;
 
 class HelloFormController extends SimpleFormController {
 
-    public function createForm() {
+    public function createForm()
+    {
         return new HelloForm();
+    }
+
+    public function register($form)
+    {
+        $email = new Email('default');
+        $email->from(['heroku.x.co@gmail.com' => 'CakePHP TEST'])
+            ->to('apple.x.co@gmail.com  ')
+            ->subject('Hello Form')
+            ->send('Hello !!');
     }
 
 }
